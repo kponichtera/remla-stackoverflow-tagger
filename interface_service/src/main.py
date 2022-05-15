2772d50344a20abc96e76115b9fdf938aa67faa8
Krzysztof Baran <krysbaran@gmail.com>
2022-05-16 00:07:54 +0200

    Add initial scaffolding

---
+++ interface_service/src/main.py
@@ -0,0 +1,43 @@
+from typing import Set, Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="Interface Service API",
+    description="Interface Service API for accessing models 🚀",
+    version="0.0.1",
+)
+
+
+@app.post('/predict')
+async def predict_tags(title : str, summary="Predict tags for StackOverflow title"):
+    """
+    Create a prediction of tags for the given StackOverflow title.
+
+    - **title**: title of the StackOverflow question
+    """
+    return {
+        "result": ["java", "OOP"],
+        "classifier": "decision tree",
+        "title": title,
+    }
+
+
+class Correction(BaseModel):
+    title: str
+    predicted: Set[str]
+    actual: Set[str]
+
+
+@app.post('/correct',
+        response_model=Correction,
+        summary="Correct the tags to the model",)
+def correct_prediction(correction : Correction):
+    """
+    Correct a prediction of tags for models to learn in the future.
+
+    - **title**: title of the StackOverflow question
+    - **predicted**: prediction of tags for the title
+    - **actual**: actual tags for the title
+    """
+    return correction
