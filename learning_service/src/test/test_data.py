4a5cdf42f035f87b74233ead3c5f3f840154f7c2
Dan Plamadeala <cornul11@gmail.com>
2022-05-26 19:51:55 +0200

    added train data test, updated requirements

--- learning_service/src/test/test_data.py
+++ learning_service/src/test/test_data.py
@@ -1,22 +1,29 @@
 import os
-import pytest
 import unittest
-import pandas as pd
+from pathlib import Path
+
+import pytest
+import tensorflow_data_validation as tfdv
+
 
 class DataTest(unittest.TestCase):
-    """Testing basic main file"""
-    
+    """Testing input data anomalies"""
+
     @pytest.fixture(autouse=True)
     def prepare_fixture(self):
         """Fixture to generate test app."""
-        dir_data = os.path.join(os.path.dirname(__file__), 'fake_data.csv')
-        self.test_df = pd.read_csv(dir_data)
-    
-    def test_no_duplicates(self):
-        self.assertEqual(len(self.test_df['id'].unique()), self.test_df.shape[0])
-        self.assertEqual(self.test_df.groupby(['percentage','id']).size().max(), 1)
-    
-    def test_value_ranges(self):
-        self.assertTrue((self.test_df['percentage']<=1).all())
-        self.assertTrue((self.test_df.groupby('name')['budget'].sum() <= 1000).all())
-        self.assertTrue(all (self.test_df['height']>=0))
+        base_dir = Path(os.path.dirname(__file__))
+        # TODO: these paths have to be changed later on when a proper workflow is set
+        self.test_data_dir = os.path.join(base_dir.parent.parent, 'data', 'train.tsv')
+        self.stats_options = tfdv.StatsOptions(enable_semantic_domain_stats=True)
+
+    def train_no_anomalies(self):
+        """Uses the tensorflow data validation library to look for anomalies in the train dataset"""
+        train_stats = tfdv.generate_statistics_from_csv(self.test_data_dir, delimiter='\t',
+                                                        stats_options=self.stats_options)
+        schema = tfdv.infer_schema(statistics=train_stats)
+        # all data points must contain a title and tags
+        tfdv.get_feature(schema, 'title').presence.min_fraction = 1.0
+        tfdv.get_feature(schema, 'tags').presence.min_fraction = 1.0
+        anomalies = tfdv.validate_statistics(statistics=train_stats, schema=schema)
+        assert 'anomaly_info' not in str(anomalies)
