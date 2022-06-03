f89900b6f6c7a2ebdac1566c94f7195cf95bfb68
Calin Georgescu <caling@protonmail.com>
2022-06-03 20:49:13 +0200

    Clean up dynaconf setup

--- interface_service/src/config.py
+++ interface_service/src/config.py
@@ -7,22 +7,6 @@ from dynaconf import Dynaconf, Validator
 settings = Dynaconf(
     # variables exported in .env as `REMLA_FOO=bar` becomes `settings.FOO == "bar"`
     envvar_prefix="REMLA",
-    settings_files=['config/settings.yaml', 'config/.secrets.yaml'],
-    # Enable layered environments
-    environments=True,
-    load_dotenv=True,
-    # to switch environments `export DYNACONF_ENV=production`
-    env_switcher="DYNACONF_ENV",
-    # custom path for .env file to be loaded
-    dotenv_path="config/.env"
+    environments=False,
+    load_dotenv=False
 )
-
-settings.validators.register(
-    # A name must exist in the default name
-    Validator("name", must_exist=True, env="default"),
-    # Custom must exist if host exists
-    Validator("custom", must_exist=True,
-              when=Validator("host", must_exist=True)),
-)
-
-settings.validators.validate()
