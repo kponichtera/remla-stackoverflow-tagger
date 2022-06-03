d7036a041e539fd116cf93d2a1907546390cf29b
Calin Georgescu <caling@protonmail.com>
2022-06-03 13:15:54 +0200

    Fix linting

--- learning_service/src/config.py
+++ learning_service/src/config.py
@@ -1,20 +1,28 @@
+"""
+Provides a configuration management object.
+"""
 
 from dynaconf import Dynaconf, Validator
 
 settings = Dynaconf(
-    envvar_prefix="REMLA",               # variables exported in .env as `REMLA_FOO=bar` becomes `settings.FOO == "bar"`
+    # variables exported in .env as `REMLA_FOO=bar` becomes `settings.FOO == "bar"`
+    envvar_prefix="REMLA",
     settings_files=['config/settings.yaml', 'config/.secrets.yaml'],
-    environments=True,                   # Enable layered environments
+    # Enable layered environments
+    environments=True,
     load_dotenv=True,
-    env_switcher="DYNACONF_ENV",         # to switch environments `export ENV_FOR_DYNACONF=production`
-    dotenv_path="config/.env"            # custom path for .env file to be loaded
+    # to switch environments `export DYNACONF_ENV=production`
+    env_switcher="DYNACONF_ENV",
+    # custom path for .env file to be loaded
+    dotenv_path="config/.env"
 )
 
 settings.validators.register(
     # A name must exist in the default name
     Validator("name", must_exist=True, env="default"),
     # Custom must exist if host exists
-    Validator("custom", must_exist=True, when=Validator("host", must_exist=True)),
+    Validator("custom", must_exist=True,
+              when=Validator("host", must_exist=True)),
 )
 
 settings.validators.validate()
