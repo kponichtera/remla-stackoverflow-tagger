c1822d3649481865f14e44515a773397a2554ce7
Cornul11 <cornul11@gmail.com>
2022-06-12 14:21:46 +0200

    reverted code styling, change port

--- src/learning_service/main.py
+++ src/learning_service/main.py
@@ -2,6 +2,6 @@ from prometheus_client import start_http_server
 from text_classification import main
 
 if __name__ == '__main__':
-    start_http_server(8000)  # exposes the Prometheus metrics on the localhost:8000/metrics URL
+    start_http_server(9010)
     while True:
         main()
